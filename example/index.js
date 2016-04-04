6d9dc323842eaf04d4a7ebebeb167489e29ed9d6
Shane Daniel <simrpgman@gmail.com>
2016-04-04 01:24:04 -0700

    example using redis

--- example/index.js
+++ example/index.js
@@ -2,20 +2,39 @@
 
 var express = require('express');
 var httpProxy = require('http-proxy');
-var cacheManager = require('cache-manager');
+var morgan = require('morgan');
 
-var cachingWrapper = require('../');
+
+var cacheManager = require('cache-manager');
+var redisStore = require('cache-manager-redis');
+
+var redisCache = cacheManager.caching({
+    store: redisStore,
+    return_buffers: true
+});
+
+var log = require('debug')('http-proxy-cached:example');
+
+var cachingMiddlewareFactory = require('../');
 
 var app = express();
 
-app.set('etag', 'strong');
+app.use(morgan('dev'));
 
 var router = express.Router();
 
-router.use(cachingWrapper({
-  proxyTarget: 'http://placekitten.com/',
-  cache: cacheManager.caching({})
+
+
+router.use(cachingMiddlewareFactory({
+  proxyTarget: 'http://localhost:8080/',
+  cache: redisCache
 }));
+
+redisCache.store.events.on('redisError', function(err) {
+  log(err);
+});
+
+
 
 app.use(router);
 
